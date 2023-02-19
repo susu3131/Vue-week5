eb8e2300fc93df130ba6d58a1041e1a4e70278ba
susu3131 <anny81327@gmail.com>
2023-02-19 11:07:57 +0800

    元件初始化完成

--- all.js
+++ all.js
@@ -2,6 +2,28 @@ const apiUrl = 'https://vue3-course-api.hexschool.io'
 const apiPath = 'susu3131'
 
 
+//區域註冊
+const productModal = {
+  data(){
+    return{
+      modal:{},
+      tempProduct:{},
+    }
+  },
+  methods:{
+    click(){
+      console.log('內層');
+      this.$emit('getTempProduct')
+    }
+  },
+  template:'#userProductModal',
+  mounted(){
+    this.modal = new bootstrap.Modal(this.$refs.modal)
+    this.modal.show()
+  }
+}
+
+
 const app = Vue.createApp({
   data() {
     return {
@@ -11,12 +33,19 @@ const app = Vue.createApp({
       }
     },
   methods: {
+    //取得資料
     getData(page=1){
       const url = `${apiUrl}/v2/api/${apiPath}/products/?page=${page}`
       axios.get(url)
         .then(res=> this.products = res.data.products)
         .catch(err=> console.log(err))
+    },
+    getTempProduct(){
+      console.log('test');
     }
+  },
+  components:{
+    productModal
   },
   mounted() {
     this.getData()
